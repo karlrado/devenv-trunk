146f63e26f95429df61b0ac4560f483b28f2948f
Karl Schultz <karl.w.schultz@gmail.com>
2024-12-28 07:17:55 -0700

    Adjust tags to karlrado

--- retag.sh
+++ retag.sh
@@ -1,11 +1,11 @@
 TAG=20240118
 
-docker manifest create happycerberus/devenv-trunk:latest --amend happycerberus/devenv-trunk:latest-arm64 --amend happycerberus/devenv-trunk:latest-amd64
-docker manifest annotate happycerberus/devenv-trunk:latest happycerberus/devenv-trunk:latest-amd64 --arch amd64
-docker manifest annotate happycerberus/devenv-trunk:latest happycerberus/devenv-trunk:latest-arm64 --arch arm64
-docker manifest push happycerberus/devenv-trunk:latest
+docker manifest create karlrado/devenv-trunk:latest --amend karlrado/devenv-trunk:latest-arm64 --amend karlrado/devenv-trunk:latest-amd64
+docker manifest annotate karlrado/devenv-trunk:latest karlrado/devenv-trunk:latest-amd64 --arch amd64
+docker manifest annotate karlrado/devenv-trunk:latest karlrado/devenv-trunk:latest-arm64 --arch arm64
+docker manifest push karlrado/devenv-trunk:latest
 
-docker manifest create happycerberus/devenv-trunk:${TAG} --amend happycerberus/devenv-trunk:${TAG}-arm64 --amend happycerberus/devenv-trunk:${TAG}-amd64
-docker manifest annotate happycerberus/devenv-trunk:${TAG} happycerberus/devenv-trunk:${TAG}-amd64 --arch amd64
-docker manifest annotate happycerberus/devenv-trunk:${TAG} happycerberus/devenv-trunk:${TAG}-arm64 --arch arm64
-docker manifest push happycerberus/devenv-trunk:${TAG}
+docker manifest create karlrado/devenv-trunk:${TAG} --amend karlrado/devenv-trunk:${TAG}-arm64 --amend karlrado/devenv-trunk:${TAG}-amd64
+docker manifest annotate karlrado/devenv-trunk:${TAG} karlrado/devenv-trunk:${TAG}-amd64 --arch amd64
+docker manifest annotate karlrado/devenv-trunk:${TAG} karlrado/devenv-trunk:${TAG}-arm64 --arch arm64
+docker manifest push karlrado/devenv-trunk:${TAG}
